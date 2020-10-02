d1ce2d02890cf6c6e3774e3ad9760c073e779661
Graham Vasquez <rescus1221@gmail.com>
2020-10-02 01:49:04 -0400

    🐛 Fixed bug with auto-login when the page loads.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Login';
@@ -8,8 +8,28 @@ import logo from './images/logo-mouth.png';
 import { Navbar } from './components/Nav';
 import { Dashboard } from './views/Dashboard';
 import { ProtectedRoute } from './components/ProtectedRoute';
+import { context } from './context/Context';
+import * as axios from 'axios';
 
 const App = () => {
+  const { state, dispatch } = useContext(context);
+  useEffect(() => {
+    const handleRefresh = async () => {
+      if (!state.auth) {
+        try {
+          dispatch({ type: 'SET_LOADING', payload: true });
+          const login = await axios.post('/api/v1/auth/refresh_token');
+          dispatch({ type: 'LOGIN', payload: login.data.accessToken });
+        } catch (err) {
+          if (err.response.status === 401) {
+          }
+        }
+      }
+    };
+    handleRefresh();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <Fragment>
       <Router>
