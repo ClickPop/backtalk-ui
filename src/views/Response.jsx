716168a097c5398d4cd760d192edd5b7c6fb0496
Graham Vasquez <rescus1221@gmail.com>
2020-10-13 15:29:29 -0400

    ✨ Displays a survey, adds questions from query string, and submits a response.

--- src/views/Response.jsx
+++ src/views/Response.jsx
@@ -1,7 +1,18 @@
 import * as axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-export const Response = ({ history }) => {
+
+const getQuery = (qStr) => {
+  if (!/\?(\S+=\S+)+/g.test(qStr)) return null;
+  const arr = qStr.slice(1, qStr.length).split('=');
+  const query = {};
+  for (let i = 0; i < arr.length; i += 2) {
+    query[arr[i]] = arr[i + 1];
+  }
+  return query;
+};
+
+export const Response = ({ history, location }) => {
   const params = useParams();
   const [survey, setSurvey] = useState({});
   const [response, setResponse] = useState({});
@@ -10,13 +21,30 @@ export const Response = ({ history }) => {
       try {
         const res = await axios.get(`/api/v1/surveys/${params.hash}`);
         setSurvey(res.data.result);
+        const queryParams = getQuery(decodeURI(location.search));
+        if (!queryParams) return;
+        const questions = await axios.post('/api/v1/question', {
+          questions: Object.keys(queryParams).map((query) => ({
+            surveyId: res.data.result.id,
+            question: {
+              prompt: query,
+              type: 'query',
+            },
+          })),
+        });
+        questions.data.results.forEach((result, i) => {
+          setResponse((r) => ({
+            ...r,
+            [result.question.id]: queryParams[result.question.prompt],
+          }));
+        });
       } catch (err) {
         console.error(err);
         // TODO add error popup
       }
     };
     getSurvey();
-  }, [params.hash]);
+  }, [params.hash, location.search]);
 
   const handleChange = (e) => {
     setResponse({ ...response, [e.target.name]: e.target.value });
@@ -46,17 +74,19 @@ export const Response = ({ history }) => {
         <div>
           <h1>{survey.title}</h1>
           {survey.questions &&
-            survey.questions.map((question) => (
-              <div key={question.id}>
-                <h2>{question.prompt}</h2>
-                <input
-                  type="text"
-                  name={question.id}
-                  value={response[question.id] ? response[question.id] : ''}
-                  onChange={handleChange}
-                />
-              </div>
-            ))}
+            survey.questions.map((question) =>
+              question.type !== 'query' ? (
+                <div key={question.id}>
+                  <h2>{question.prompt}</h2>
+                  <input
+                    type="text"
+                    name={question.id}
+                    value={response[question.id] ? response[question.id] : ''}
+                    onChange={handleChange}
+                  />
+                </div>
+              ) : null,
+            )}
           <button name="submit" onClick={handleSubmit}>
             Submit
           </button>
