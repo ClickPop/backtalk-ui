8d0fe17ab234c636e592c2734bc8a4d3e44e69cd
Graham Vasquez <rescus1221@gmail.com>
2020-11-12 23:25:12 -0500

    🐛 Fixed a few bugs

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,13 @@
 import * as axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
 import { context } from '../context/Context';
 
 const NavItem = ({ pathname, text, className, onClick }) => {
   return (
     <li className="nav-item" onClick={onClick}>
-      <Link to={{ pathname }} className={className}>
-        {text}
-      </Link>
+      <Button pathname={pathname} className={className} text={text} />
     </li>
   );
 };
