dd5b401575bf30fd9093e10c7faf688b28e6cf2d
chris <chrisvqz@gmail.com>
2020-11-25 21:29:38 -0500

    Add feedback float component

---
+++ src/components/FeedbackFloat.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MessageSquare } from 'react-feather';
+
+const FeedbackFloat = () => {
+  return (
+    <a
+      href="https://backtalk.io/survey/ReG94d4J"
+      target="_blank"
+      class="feedbackFloat"
+    >
+      Feedback <MessageSquare size={16} />
+    </a>
+  );
+};
+
+export { FeedbackFloat };
