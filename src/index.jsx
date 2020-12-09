7f58c9745703e9e18bafc8c2b6251a15c8aff466
Graham Vasquez <rescus1221@gmail.com>
2020-12-09 00:15:25 -0500

    ✨ Added ResponseMap using mapbox-gl

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/index.scss';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import App from './App';
 import 'bootstrap';
 import 'popper.js';
